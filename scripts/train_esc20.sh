5074025f861571fbab5fac1ca9e713a86a3ce9f8
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2021-01-20 13:55:44 -0600

    added scripts for esc20

---
+++ scripts/train_esc20.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./train.py --epochs 200 --optimizer Adam --lr 0.001 --deterministic --compress schedule_esc20.yaml --model ai85kws20netv3 --dataset ESC_20 --confusion --device MAX78000 "$@"
